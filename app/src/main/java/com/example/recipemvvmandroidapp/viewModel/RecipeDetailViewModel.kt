571d645a75f914970430d9a7044cc3a93035ca81
Toan Pham <tpham61@calpoly.edu>
2021-05-14 19:49:02 -0500

    implement recipeDetailView

--- app/src/main/java/com/example/recipemvvmandroidapp/viewModel/RecipeDetailViewModel.kt
+++ app/src/main/java/com/example/recipemvvmandroidapp/viewModel/RecipeDetailViewModel.kt
@@ -1,14 +1,56 @@
 package com.example.recipemvvmandroidapp.viewModel
 
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import com.example.recipemvvmandroidapp.dependency.Dependency
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.recipemvvmandroidapp.domain.model.Recipe
+import com.example.recipemvvmandroidapp.domain.useCase.GetRecipeDetailUseCase
+import com.example.recipemvvmandroidapp.domain.useCase.UseCaseResult
+import com.example.recipemvvmandroidapp.domain.useCase.getRecipeDetailUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RecipeDetailViewModel(
-
+    private val getRecipeDetailUseCase: GetRecipeDetailUseCase
 ): ViewModel(){
     companion object{
         var instance: RecipeDetailViewModel? = null
+    }
+
+    data class RecipeForDetailViewInViewModel(
+        val title: String,
+        val featuredImage: String,
+        val ingredients: List<String>
+    )
+
+    var recipeForDetailView: MutableState<RecipeForDetailViewInViewModel> = mutableStateOf(
+        RecipeForDetailViewInViewModel(
+            title = "",
+            featuredImage = "",
+            ingredients = listOf()
+        )
+    )
+
+    fun onLaunch(recipeId: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = getRecipeDetailUseCase.execute(recipeId)
+            when(result)
+            {
+                is UseCaseResult.Success -> recipeForDetailView.value = result.resultValue.let{
+                    RecipeForDetailViewInViewModel(
+                        title = it.title,
+                        featuredImage = it.featuredImage,
+                        ingredients = it.ingredients
+                    )
+                }
+                is UseCaseResult.Error -> Log.d("Debug: RecipeDetailViewModel",
+                    result.exception.toString()
+                )
+            }
+        }
     }
 }
 
@@ -16,7 +58,7 @@ fun Dependency.ViewModel.recipeDetailViewModel(): RecipeDetailViewModel
 {
     if(RecipeDetailViewModel.instance == null)
     {
-        RecipeDetailViewModel.instance = RecipeDetailViewModel()
+        RecipeDetailViewModel.instance = RecipeDetailViewModel(useCase.getRecipeDetailUseCase())
     }
     return RecipeDetailViewModel.instance!!
 }
