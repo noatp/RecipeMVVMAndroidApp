638748107f5d042860b28d04a9f2f767f698e1d4
Toan Pham <tpham61@calpoly.edu>
2021-05-23 14:13:01 -0500

    centralized all routing logic

--- app/src/main/java/com/example/recipemvvmandroidapp/router/RouterController.kt
+++ app/src/main/java/com/example/recipemvvmandroidapp/router/RouterController.kt
@@ -1,6 +1,17 @@
 package com.example.recipemvvmandroidapp.router
 
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import com.example.recipemvvmandroidapp.view.RecipeDetailView
+import com.example.recipemvvmandroidapp.view.tabView.DiscoveryView
+import com.example.recipemvvmandroidapp.view.tabView.SearchRecipeView
+import com.example.recipemvvmandroidapp.viewModel.DiscoveryViewModel
+import com.example.recipemvvmandroidapp.viewModel.RecipeDetailViewModel
+import com.example.recipemvvmandroidapp.viewModel.SearchRecipeViewModel
 
 class RouterController(
     val tabController: NavHostController,
@@ -32,5 +43,55 @@ class RouterController(
         modalController.navigate(
             route = ViewDestination.RecipeDetailView.route + "/$recipeId"
         )
+    }
+
+    @Composable
+    fun AttachNavHostForTabController(){
+        NavHost(navController = tabController, startDestination = TabViewDestination.Search.route)
+        {
+            TabViewDestination.values().map { tabViewDestination: TabViewDestination ->
+                composable(tabViewDestination.route) {
+                    when(tabViewDestination)
+                    {
+                        TabViewDestination.Search -> {
+                            val viewModel = hiltViewModel<SearchRecipeViewModel>(backStackEntry = it)
+                            SearchRecipeView(this@RouterController, viewModel)
+                        }
+                        TabViewDestination.Discovery -> {
+                            val viewModel = hiltViewModel<DiscoveryViewModel>(backStackEntry = it)
+                            DiscoveryView(this@RouterController, viewModel)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun AttachNavHostForModalController(){
+        NavHost(navController = modalController, startDestination = "blank")
+        {
+            composable(route = "blank") {}
+            ViewDestination.values().map{ viewDestination: ViewDestination ->
+                composable(
+                    route = viewDestination.route + "/{${viewDestination.arguments.first}}",
+                    arguments = listOf(
+                        navArgument(viewDestination.arguments.first){
+                            type = viewDestination.arguments.second}
+                    )
+                ) {
+                    when(viewDestination)
+                    {
+                        ViewDestination.RecipeDetailView -> {
+                            val viewModel = hiltViewModel<RecipeDetailViewModel>(backStackEntry = it)
+                            RecipeDetailView(
+                                recipeId = it.arguments?.getInt(viewDestination.arguments.first)!!,
+                                recipeDetailViewModel = viewModel
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/recipemvvmandroidapp/view/HomeView.kt
+++ app/src/main/java/com/example/recipemvvmandroidapp/view/HomeView.kt
@@ -7,21 +7,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.navArgument
 import com.example.recipemvvmandroidapp.router.RouterController
 import com.example.recipemvvmandroidapp.router.TabViewDestination
-import com.example.recipemvvmandroidapp.router.ViewDestination
-import com.example.recipemvvmandroidapp.view.tabView.DiscoveryView
-import com.example.recipemvvmandroidapp.view.tabView.SearchRecipeView
 import com.example.recipemvvmandroidapp.view.viewComponent.NavTabRow
-import com.example.recipemvvmandroidapp.viewModel.DiscoveryViewModel
-import com.example.recipemvvmandroidapp.viewModel.RecipeDetailViewModel
-import com.example.recipemvvmandroidapp.viewModel.SearchRecipeViewModel
 
 @Composable
 fun CreateHomeView(
@@ -43,51 +32,11 @@ fun CreateHomeView(
             )
         } },
         content = {
-            NavHost(navController = router.tabController, startDestination = TabViewDestination.Search.route)
-            {
-                TabViewDestination.values().map { tabViewDestination: TabViewDestination ->
-                    composable(tabViewDestination.route) {
-                        when(tabViewDestination)
-                        {
-                            TabViewDestination.Search -> {
-                                val viewModel = hiltViewModel<SearchRecipeViewModel>(backStackEntry = it)
-                                SearchRecipeView(router, viewModel)
-                            }
-                            TabViewDestination.Discovery -> {
-                                val viewModel = hiltViewModel<DiscoveryViewModel>(backStackEntry = it)
-                                DiscoveryView(router, viewModel)
-                            }
-                        }
-                    }
-                }
-            }
+            router.AttachNavHostForTabController()
         }
     )
 
-    NavHost(navController = router.modalController, startDestination = "blank")
-    {
-        composable(route = "blank") {}
-        ViewDestination.values().map{ viewDestination: ViewDestination ->
-            composable(
-                route = viewDestination.route + "/{${viewDestination.arguments.first}}",
-                arguments = listOf(
-                    navArgument(viewDestination.arguments.first){
-                        type = viewDestination.arguments.second}
-                )
-            ) {
-                when(viewDestination)
-                {
-                    ViewDestination.RecipeDetailView -> {
-                        val viewModel = hiltViewModel<RecipeDetailViewModel>(backStackEntry = it)
-                        RecipeDetailView(
-                            recipeId = it.arguments?.getInt(viewDestination.arguments.first)!!,
-                            recipeDetailViewModel = viewModel
-                        )
-                    }
-                }
-            }
-        }
-    }
+    router.AttachNavHostForModalController()
 }
 
 @Composable
