5db6c9113f28bdc0f232471588a151a2105357c5
Toan Pham <tpham61@calpoly.edu>
2021-06-04 01:46:00 -0500

    handling errors in RecipeRepository

--- app/src/main/java/com/example/recipemvvmandroidapp/data/repositoryImplementation/recipeRepository/RecipeRepository.kt
+++ app/src/main/java/com/example/recipemvvmandroidapp/data/repositoryImplementation/recipeRepository/RecipeRepository.kt
@@ -1,5 +1,6 @@
 package com.example.recipemvvmandroidapp.data.repositoryImplementation.recipeRepository
 
+import android.util.Log
 import com.example.recipemvvmandroidapp.data.remote.RecipeNetworkService
 import com.example.recipemvvmandroidapp.domain.model.RecipeDTO
 import com.example.recipemvvmandroidapp.domain.util.RecipeDTOMapper
@@ -7,25 +8,32 @@ import com.example.recipemvvmandroidapp.domain.util.recipeDTOMapper
 import com.example.recipemvvmandroidapp.dependency.Dependency
 import com.example.recipemvvmandroidapp.dependency.recipeService
 import com.example.recipemvvmandroidapp.domain.repositoryInterface.RecipeRepositoryInterface
+import java.lang.Exception
 
 class RecipeRepository(
     private val recipeNetworkService: RecipeNetworkService,
     private val recipeDTOMapper: RecipeDTOMapper
 ): RecipeRepositoryInterface {
     override suspend fun getRecipeById(id: Int): RecipeDTO {
-        return recipeDTOMapper.mapDomainModelToDTO(
-            recipeNetworkService.getRecipeById(id)
-        )
+        try{
+            return recipeDTOMapper.mapDomainModelToDTO(
+                recipeNetworkService.getRecipeById(id)
+            )
+        } catch (exception: Exception){
+            Log.d("Rethrow exception in RecipeRepository: getRecipeById", "$exception")
+            throw exception
+        }
     }
 
     override suspend fun searchForRecipes(page: Int, query: String): List<RecipeDTO> {
-        return recipeDTOMapper.mapListDomainModelToListDTO(
-            recipeNetworkService.searchForRecipes(page, query)
-        )
-    }
-
-    companion object{
-        const val API_PAGE_SIZE = 30
+        try{
+            return recipeDTOMapper.mapListDomainModelToListDTO(
+                recipeNetworkService.searchForRecipes(page, query)
+            )
+        } catch (exception: Exception){
+            Log.d("Rethrow exception in RecipeRepository: searchForRecipes", "$exception")
+            throw exception
+        }
     }
 }
 
